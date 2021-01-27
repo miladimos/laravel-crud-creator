6a95f2868621f0c254e16f314bb000458c22e82a
miladimos <milad.jafari6210@gmail.com>
2021-01-28 03:15:54 +0330

    Add createWebCrud method

--- src/CrudCreator.php
+++ src/CrudCreator.php
@@ -17,16 +17,32 @@ class CrudCreator
         $modelNamespace = (new static)->getModelNamespace($modelName);
         // dd($apiContollerNamespace);
         $template = str_replace(
-            ['{{ $modelName }}', '{{ $apiControllerNamespace }}'],
-            [$modelName, $apiContollerNamespace],
+            ['{{ $modelName }}', '{{ $modelNamespace }}', '{{ $apiControllerNamespace }}'],
+            [$modelName, $modelNamespace, $apiContollerNamespace],
             (new static)->getApiControllerStub($modelName)
         );
 
 
-        if (!file_exists($path=base_path('/App/Providers/CrudCreatorServiceProvider.php')))
+        if (!file_exists($path = base_path('/App/Providers/CrudCreatorServiceProvider.php')))
             file_put_contents(base_path('/App/Providers/CrudCreatorServiceProvider.php'), $template);
 
         file_put_contents(base_path("/App/Repositories/{$modelName}CrudCreator.php"), $template);
     }
 
+    public static function createWebCrud($modelName)
+    {
+        $webContollerNamespace = (new static)->getWebControllerDefaultNamespace();
+        $modelNamespace = (new static)->getModelNamespace($modelName);
+        $template = str_replace(
+            ['{{ $modelName }}', '{{ $modelNamespace }}', '{{ $webControllerNamespace }}'],
+            [$modelName, $modelNamespace, $webContollerNamespace],
+            (new static)->getWebControllerStub($modelName)
+        );
+
+
+        if (!file_exists($path = base_path('/App/Providers/CrudCreatorServiceProvider.php')))
+            file_put_contents(base_path('/App/Providers/CrudCreatorServiceProvider.php'), $template);
+
+        file_put_contents(base_path("/App/Repositories/{$modelName}CrudCreator.php"), $template);
+    }
 }
